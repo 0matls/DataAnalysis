1653fe36c1f33b286fe9eb84144ee8d6540795f1
0matls <88787230+0matls@users.noreply.github.com>
2021-11-25 01:31:01 +0200

    Create ex3_ch2.m

---
+++ chapter2/ex3_ch2.m
@@ -0,0 +1,17 @@
+clear; clc; close all;
+
+%%  Data Analysis
+%   1st assignment 19-10-21 ex 3
+
+meanValues = [0 0];
+Sigma = [1 2;2 10]; %must be a symmetric positive semi-definite matrix
+% also 2 != 0 so the variables are not independent
+R = mvnrnd(meanValues,Sigma,100000);
+varX = var(R(:,1));
+varY = var(R(:,2));
+varXplusY = varX + varY;
+ 
+sumVars = var(R(:,1)+R(:,2));
+fprintf('Var[X+Y] = %3.3f, but Var[X] + Var[Y] = %3.3f + %3.3f = %3.3f \n', sumVars,varX,...
+    varY,varXplusY);
+ 
